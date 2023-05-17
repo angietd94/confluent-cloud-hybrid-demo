2ab48c3d9cdf741114ead139affef82d68c2a50c
angietd94 <76002148+angietd94@users.noreply.github.com>
2023-05-17 14:59:06 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-export CONFLUENT_USERNAME="ataccadughetti+ubb@confluent.io"
+export CONFLUENT_USERNAME="****"
 export CONFLUENT_PASSWORD="***"
 #CLOUD API KEYS
 
-export CLOUD_KEY="XHLDXYZXUVJEKAEK"
-export CLOUD_SECRET="iAmpwYBClKWi4/KQSqw6Cz60/IWXg5cixIiUcplriz//RYRYt1Tmjx2c/sIPm/E1
-D2X8I5Nj6PZk1tYFUirfdxo9793hyU0Sbaw1GSEwH/GF7IZzyaEyl9RFzs+z1reo"
-export ORG_ID="6567aa66-3926-4012-8364-9051f660cef8"
-export ENVID="env-rrr650"
+export CLOUD_KEY="*****"
+export CLOUD_SECRET="*****"
+export ORG_ID="***-***-***-***-****"
+export ENVID="env-****"
 export EMAIL=""
 export ACCOUNTPW=""
 
 
-export CLUSTERID="lkc-mvokz2"
+export CLUSTERID="lkc-***"
 ##only needed if creating a new cluster
 #export CLUSTERTYPE="basic"
 #export CLUSTERNAME="self-managed-demo"
@@ -21,15 +20,16 @@ export CLUSTERID="lkc-mvokz2"
 export GEO="eu"
 export REGION="eu-central-1"
 
-export CLUSTERAPIKEY="URXPXUAHDVBAOAEZ"
-export CLUSTERAPISECRET="D2X8I5Nj6PZk1tYFUirfdxo9793hyU0Sbaw1GSEwH/GF7IZzyaEyl9RFzs+z1reo"
+export CLUSTERAPIKEY="*****"
+export CLUSTERAPISECRET="******"
 
+#for example, aws or gcp
 export CLOUDPROVIDER="aws"
 
-export SR_API_KEY="FBCFSJAUYYGN47FR"
-export SR_API_SECRET="I/dnygHuuamZvj0VwACCN+WmFCZunxnQJVLLqUzH8AMbocCneTFO2AvUI8nyZbIF"
-export BOOTSTRAP_SERVERS="pkc-zpjg0.eu-central-1.aws.confluent.cloud:9092"
-export SCHEMA_REGISTRY_URL="https://psrc-8vyvr.eu-central-1.aws.confluent.cloud"
+export SR_API_KEY="****"
+export SR_API_SECRET="****"
+export BOOTSTRAP_SERVERS="*****:****"
+export SCHEMA_REGISTRY_URL="****+"
 
 
 export SASL_JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=$CLUSTERAPIKEY password=$CLUSTERAPISECRET;"
@@ -40,8 +40,8 @@ export SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO=$SR_API_KEY:$SR_API_SECRET
 
 ## CLICKHOUSE DATA
 
-export TOPIC="pageviews"
+export TOPIC="*****"
 export DATABASE_NAME="default"
-export DATABASE_PASSWORD="egNKoxgICWr~8"
-export DATABASE_HOSTNAME="uzkee0x1rb.eu-west-1.aws.clickhouse.cloud"
-export CLICKHOUSE_HOSTNAME=""
+export DATABASE_PASSWORD="*****"
+export DATABASE_HOSTNAME="******"
+export CLICKHOUSE_HOSTNAME="****"
